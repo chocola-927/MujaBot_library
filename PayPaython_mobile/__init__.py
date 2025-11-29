c5461bae7afac6dd44723366d9df867ce54ffa92
chocola_927 <ktgw0218@gmail.com>
2025-11-29 19:08:12 +0900

    Update __init__.py

--- PayPaython_mobile/__init__.py
+++ PayPaython_mobile/__init__.py
@@ -1,3 +1,3 @@
-from .main import PayPay, PayPayLoginError, PayPayError, PayPayNetWorkError, AwsWafException
+from .main import PayPay, PayPayLoginError, PayPayError, PayPayNetWorkError
 __version__ = '3.0.0'
 __url__ = 'https://github.com/chocola-927/MujaBot_library/'
